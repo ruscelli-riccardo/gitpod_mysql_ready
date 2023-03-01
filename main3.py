2e1d89ca82058354faae79611e503e04c526059b
ruscelli-riccardo <ruscelli.riccardo.angelo@iisgalvanimi.edu.it>
2023-03-01 11:59:02 +0000

    1 marzo

--- main3.py
+++ main3.py
@@ -9,13 +9,16 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-sql = "INSERT INTO Mammiferi (ID, Nome_proprio, Razza, Peso, Età) VALUES (%s, %s)"
+sql = "INSERT INTO Mammiferi (ID, Nome_proprio, Razza, Peso, Età) VALUES (%s, %s, %s, %s, %s)"
 val = [
   ( 1, 'Bommo', 'Capra', 50, 19),
   ( 2, 'Pippo', 'Lupo', 34, 5),
   ( 3, 'Pluto', 'Leone', 100, 34),
   ( 4, 'Luca', 'Elefante', 500, 2),
-  ( 5, 'Gianni', 'Scimmia', 20, 98),
+  ( 5, 'Gianni', 'Scimmia', 20, 98)
 ]
 
+mycursor.executemany(sql, val)
+
 mydb.commit()
+
