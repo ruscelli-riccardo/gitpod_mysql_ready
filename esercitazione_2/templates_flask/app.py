a8f11186cb7b98d33ec3448e4b81cdacf6c7906e
ruscelli-riccardo <ruscelli.riccardo.angelo@iisgalvanimi.edu.it>
2023-04-13 11:00:52 +0000

    13 aprile

--- esercitazione_2/templates_flask/app.py
+++ esercitazione_2/templates_flask/app.py
@@ -1,10 +1,24 @@
 from flask import render_template
 from flask import Flask
-
+import mysql.connector
 
 app = Flask(__name__)
 
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="CLASH_ROYALE"
+)
+mycursor = mydb.cursor()
 
 @app.route('/')
 def hello():
-    return render_template('hello.html', name='Riccardo')
+    return render_template('clash_units.html', name='Riccardo')
+
+
+@app.route('/units')
+def unitList():
+    mycursor.execute("SELECT * FROM Clash_Unit")
+    myresult = mycursor.fetchall()
+    return render_template('clash_units.html', units=myresult)
